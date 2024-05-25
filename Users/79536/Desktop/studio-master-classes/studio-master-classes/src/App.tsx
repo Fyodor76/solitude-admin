0f0090801a49d882a06c28d353eaacb47b4f3bd7
Fyodor76 <100220038+Fyodor76@users.noreply.github.com>
2024-05-25 21:17:41 +0300

    добавлен код для header

---
+++ Users/79536/Desktop/studio-master-classes/studio-master-classes/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.scss'
+import { Header } from './stages/header/Header'
+
+function App() {
+  return (
+    <div className='main'>
+      <Header/>
+    </div>
+  )
+}
+
+export default App
