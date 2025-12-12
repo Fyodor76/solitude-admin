3be06364d3dd1b9b2759428d0c0a3043fc43d218
Ernesta-Vas <114830530+Ernesta-Vas@users.noreply.github.com>
2025-12-12 10:49:03 +0300

    add ts

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://api.solitude-store.ru",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        secure: false,
+      },
+    },
+  },
+  esbuild: {
+    loader: "tsx",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+  },
+});
