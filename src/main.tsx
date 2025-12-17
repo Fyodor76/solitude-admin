023fca813d2aee7067c79487a0d1e80580a0d3cc
Nadya545 <nadyamishina48@gmail.com>
2025-12-17 14:33:09 +0300

    добавила prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Router } from "./app/router/Router";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Router } from './app/router/Router';
 
 const router = createBrowserRouter(Router);
 
-createRoot(document.getElementById("root") as HTMLElement).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>
