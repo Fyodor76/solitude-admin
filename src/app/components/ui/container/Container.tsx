c54609fce6e504a122e3c89a8f8d7eb2950e7115
Nadya545 <nadyamishina48@gmail.com>
2025-12-19 09:45:10 +0300

    переделала пропсы через дефис и добавита типы для classnames

--- src/app/components/ui/container/Container.tsx
+++ src/app/components/ui/container/Container.tsx
@@ -2,22 +2,23 @@ import { ReactNode } from "react";
 import  "./Container.scss";
 import cn from "classnames"
 
+
 interface ContainerProps {
   children: ReactNode;
-  fullWidth?: boolean;
-  fullHeight?: boolean;
-  fullScreen?: boolean;
-  noPadding?: boolean;
+  "full-width"?: boolean;
+  "full-height"?: boolean;
+  "full-screen"?: boolean;
+  "no-padding"?: boolean;
   as?: React.ElementType;
   className?: string;
 }
 
 const Container: React.FC<ContainerProps> = ({
   children,
-  fullWidth = false,
-  fullHeight = false,
-  fullScreen = false,
-  noPadding = false,
+  "full-width": fullWidth = false,
+  "full-height": fullHeight = false,
+  "full-screen": fullScreen = false,
+  "no-padding": noPadding = false,
   as: Component = "div",
   className = "",
   ...props
@@ -25,10 +26,10 @@ const Container: React.FC<ContainerProps> = ({
   const containerClasses = cn(
     "container",
     { 
-   "container--fullWidth":fullWidth,
-    "container--fullHeight":fullHeight,
-    "container--fullScreen":fullScreen,
-    "container--noPadding":noPadding,
+   "container--full-width":fullWidth,
+    "container--full-height":fullHeight,
+    "container--full-screen":fullScreen,
+    "container--no-padding":noPadding,
   },
     className,
   )
