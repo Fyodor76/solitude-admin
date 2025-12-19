7652c73f82bfdaab5a635b36938e20a512d90b42
Nadya545 <nadyamishina48@gmail.com>
2025-12-19 10:54:30 +0300

    дописала type.ts

--- src/app/lib/api/type.ts
+++ src/app/lib/api/type.ts
@@ -1,9 +1,30 @@
-import { AxiosInstance, AxiosRequestConfig } from 'axios'
+import { AxiosRequestConfig } from 'axios'
 
-export interface CustomApiClient extends AxiosInstance{
-    get<T>(url: string, config?:AxiosRequestConfig): Promise<T>
-  post<T>(url: string, data?: any, config?:AxiosRequestConfig): Promise<T>
-  put<T>(url: string, data?: any, config?:AxiosRequestConfig ): Promise<T>
-  patch<T>(url: string, data?: any, config?:AxiosRequestConfig ): Promise<T>
-  delete<T>(url: string, config?:AxiosRequestConfig ): Promise<T>
+export interface ApiResponse<T,M>{
+  success: boolean;
+  data:T;
+  meta:M;
+}
+
+export interface CustomApiClient {
+    get<T=any, D=any>(url: string, config?:AxiosRequestConfig<D>): Promise<T>
+  post<T=any, D=any>(url: string, data?: any, config?:AxiosRequestConfig<D>): Promise<T>
+  put<T=any, D=any>(url: string, data?: any, config?:AxiosRequestConfig<D> ): Promise<T>
+  patch<T=any, D=any>(url: string, data?: any, config?:AxiosRequestConfig<D> ): Promise<T>
+  delete<T=any, D=any>(url: string, config?:AxiosRequestConfig<D> ): Promise<T>
+  head<T = any, D = any>(url: string, config?: AxiosRequestConfig<D>): Promise<T>
+
+  options<T = any, D = any>(url: string, config?: AxiosRequestConfig<D>): Promise<T>
+
+  request<T = any, D = any>(config: AxiosRequestConfig<D>): Promise<T>
+
+  getUri(config?: AxiosRequestConfig): string
+
+  defaults: AxiosRequestConfig
+  interceptors: {
+    request: any
+    response: any
+  }
+
+  [key: string]: any
 }
