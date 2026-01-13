d73ca4585e8e51479c9b74f8221a584fd549e1cf
Nadya545 <nadyamishina48@gmail.com>
2026-01-13 13:57:17 +0300

    применила useErrorHandler

--- src/shared/lib/hooks/useImageState.ts
+++ src/shared/lib/hooks/useImageState.ts
@@ -3,17 +3,19 @@ import { useState } from 'react'
 import { imgUpload } from '../api/upload-files/uploadFiles'
 import { useDeleteImage, useUploadImage } from '../api/upload-files/useMutationImage'
 import { useGetFileUrlById } from '../api/upload-files/useQueryImage'
+import { useErrorHandler } from './useErrorHandler'
 
 export const useImageState = () => {
   const [images, setImages] = useState<imgUpload[]>([])
   const upload = useUploadImage()
   const deleteImg = useDeleteImage()
   const getUrl = useGetFileUrlById()
+  const { catchErrors } = useErrorHandler()
 
   const isLoading = upload.isLoading || deleteImg.isLoading || getUrl.isLoading
 
-  const uploadImage = async (file: File, folder?: string) => {
-    try {
+  const uploadImage = (file: File, folder?: string) => {
+    return catchErrors(async () => {
       const result = await upload.uploadImage(file, folder)
       const newImage: imgUpload = {
         fileId: result.data.fileId,
@@ -21,33 +23,25 @@ export const useImageState = () => {
       }
       setImages(prev => [...prev, newImage])
       return result
-    } catch (error) {
-      console.error('Ошибка загрузки:', error)
-      throw error
-    }
+    }, 'useImageState.uploadImage')
   }
 
-  const deleteImage = async (fileId: string) => {
-    try {
-      await deleteImg.deleteImage(fileId)
+  const deleteImage = (fileId: string) => {
+    return catchErrors(async () => {
+      const result = await deleteImg.deleteImage(fileId)
       setImages(prev => prev.filter(img => img.fileId !== fileId))
-    } catch (error) {
-      console.error('Ошибка удаления:', error)
-      throw error
-    }
+      return result
+    }, 'useImageState.deleteImage')
   }
 
-  const getImageUrlById = async (fileId: string) => {
-    try {
+  const getImageUrlById = (fileId: string) => {
+    return catchErrors(async () => {
       const result = await getUrl.getUrlById(fileId)
       if (result.data) {
         return result.data.data.url
       }
       throw new Error('Url не найден')
-    } catch (error) {
-      console.error('Ошибка получения Url', error)
-      throw error
-    }
+    }, 'useImageState.getImageUrlById')
   }
   return {
     images,
