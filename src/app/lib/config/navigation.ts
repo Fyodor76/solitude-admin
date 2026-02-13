b2481d081ac8bcd99abb104d9ba071d118e519d1
Nadya545 <nadyamishina48@gmail.com>
2026-02-13 14:21:17 +0300

    продолжаю работу

---
+++ src/app/lib/config/navigation.ts
@@ -0,0 +1,7 @@
+export const ROUTES = {
+  HOME: { label: 'Главная', path: '/' },
+  PRIVACY: { label: 'Политика конфиденциальности', path: '/privacy-policy' },
+  CONSENT: { label: 'Согласие на обработку персональных данных', path: '/data-consent' },
+  COLLECTION: { label: 'Коллекция', path: '/collection' },
+  CART: { label: 'Корзина', path: '/cart' },
+} as const
