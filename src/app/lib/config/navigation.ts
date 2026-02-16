f66fa38847aae4f1ca38c712b0e9cce46d37949e
Nadya545 <nadyamishina48@gmail.com>
2026-02-16 14:06:53 +0300

    удалила ненужный компонент

--- src/app/lib/config/navigation.ts
+++ src/app/lib/config/navigation.ts
@@ -2,6 +2,4 @@ export const ROUTES = {
   HOME: { label: 'Главная', path: '/' },
   PRIVACY: { label: 'Политика конфиденциальности', path: '/privacy-policy' },
   CONSENT: { label: 'Согласие на обработку персональных данных', path: '/data-consent' },
-  COLLECTION: { label: 'Коллекция', path: '/collection' },
-  CART: { label: 'Корзина', path: '/cart' },
 } as const
